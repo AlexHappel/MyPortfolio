55058d80afe0a12de3de47933822a5309937feeb
Alexander Happel <Alex.happel90@gmail.com>
2024-07-14 11:47:47 -0400

    added to Header, Footer, Navigation and App script

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () =(
+  <Router>
+    <Header />
+    <main>
+      <Switch>
+        <Route exact path="/" component={About} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/resume" component={contact} />   
+      </Switch>
+    </main>
+  </Router>
+);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
---
+++ react/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer>
+        <a href="https://github.com/AlexHappel" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://www.linkedin.com/in/alexander-happel" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+    </footer>
+);
+
+export default Footer;
