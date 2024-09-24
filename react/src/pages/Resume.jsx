3ad4a688e8859afd809544c4ff0ac0525484ad47
Alexander Happel <Alex.happel90@gmail.com>
2024-09-24 18:46:30 -0400

    added resume to assets

--- react/src/pages/Resume.jsx
+++ react/src/pages/Resume.jsx
@@ -5,7 +5,7 @@ const Resume = () => (
     <div className="resume-container">
         <div className="resume-content">
             <h2>Resume</h2>
-            <p>Download my resume <a href="/path-to-resume.pdf" download>here</a>.</p>
+            <p>Download my resume <a href="react\src\assets\Resume-Happel.pdf" download>here</a>.</p>
             <h3>Proficiencies</h3>
             <ul>
                 <li>HTML</li>
